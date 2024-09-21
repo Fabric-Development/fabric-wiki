4ed0cf21e64ad2cc2c147015001df51e4bb8daf0
The Darsh <129121284+its-darsh@users.noreply.github.com>
2024-09-21 07:07:19 +0300

    fix: 404 when loading public assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/fabric-wiki",
+  trailingSlash: "always",
   integrations: [
     starlight({
       title: "Fabric",
