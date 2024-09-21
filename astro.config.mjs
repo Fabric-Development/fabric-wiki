fe211f6e3ad151a88463962b599d5f57e4894d23
The Darsh <129121284+its-darsh@users.noreply.github.com>
2024-09-21 06:50:04 +0300

    initialize repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,67 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs with Tailwind',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  integrations: [
+    starlight({
+      title: "Fabric",
+      editLink: {
+        baseUrl: "https://github.com/Fabric-Development/fabric-wiki/edit/",
+      },
+      logo: {
+        dark: "./src/assets/logo-dark.svg",
+        light: "./src/assets/logo-light.svg",
+        replacesTitle: false,
+      },
+      social: {
+        github: "https://github.com/Fabric-Development/fabric",
+        discord: "https://discord.gg/3sDbYc9SZP",
+      },
+      sidebar: [
+        {
+          label: "Introduction",
+          autogenerate: { directory: "introduction" },
+        },
+
+        {
+          label: "Guide",
+          autogenerate: { directory: "guide" },
+        },
+
+        {
+          label: "Widgets",
+          autogenerate: { directory: "widgets" },
+        },
+      ],
+
+      customCss: [
+        "./src/tailwind.css",
+        "./src/styles/custom.css",
+
+        "@fontsource/jost/100.css",
+        "@fontsource/jost/200.css",
+        "@fontsource/jost/300.css",
+        "@fontsource/jost/400.css",
+        "@fontsource/jost/600.css",
+        "@fontsource/jost/700.css",
+        "@fontsource/jost/800.css",
+        "@fontsource/jost/900.css",
+
+        "@fontsource/readex-pro/200.css",
+        "@fontsource/readex-pro/300.css",
+        "@fontsource/readex-pro/400.css",
+        "@fontsource/readex-pro/600.css",
+        "@fontsource/readex-pro/700.css",
+      ],
+    }),
+    tailwind({ applyBaseStyles: false }),
+    react({
+      include: "./src/components/*.[jsx|tsx]",
+    }),
+  ],
 });
